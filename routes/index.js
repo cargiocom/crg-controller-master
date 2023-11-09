c27fd4533d16ec159e0c3bc7b8e43c9f68770b4e
Cargio <141646415+cargiocom@users.noreply.github.com>
2023-11-09 06:11:49 -0800

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,16 @@
-const IPFS = require('ipfs')
+const CRGS = require('crgs')
 const multer = require('multer')
 const storage = multer.memoryStorage()
 const upload = multer({ storage })
 
 module.exports = (app) => {
-  const node =  new IPFS()
+  const node =  new CRGS()
 
   app.post('/file', upload.single('file'), async (req, res, next) => {
     await node.ready
 
     if (!req.file) {
-      return res.status(400).send({ message: 'You should upload a file' })
+      return res.status(400).send({ message: '400' })
     }
 
     const file = await node.add(req.file.buffer)
