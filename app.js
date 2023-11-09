9202c7b07eceaf07b5f74106b349e18bc74c0459
Cargio <141646415+cargiocom@users.noreply.github.com>
2023-11-09 06:09:54 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,30 @@
+const createError = require('http-errors')
+const express = require('express')
+const cookieParser = require('cookie-parser')
+const logger = require('morgan')
+const cors = require('cors')
+
+const indexRouter = require('./routes/index')
+
+const app = express()
+
+app.use(logger('dev'))
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+
+indexRouter(app)
+
+app.use(function(req, res, next) {
+  next(createError(404))
+})
+
+app.use(function(err, req, res, next) {
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
+  res.status(err.status || 500)
+  res.send('error')
+})
+
+module.exports = app
