c5bb4ff45acdcb8cd7360d3b852451e8ced0bfb5
Cargio <141646415+cargiocom@users.noreply.github.com>
2024-03-05 00:49:00 -0800

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-4const createError = require('http-errors')
+44const createError = require('http-errors')
 const express = require('express')
 const cookieParser = require('cookie-parser')
 const logger = require('crglog')
